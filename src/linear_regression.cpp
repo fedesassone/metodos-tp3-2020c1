c97484597dbc48c34e10b86ff0143cffbb565080
Gian150 <gianfranco376@gmail.com>
2020-07-17 20:39:46 -0300

    Multiple linear regression

--- src/linear_regression.cpp
+++ src/linear_regression.cpp
@@ -8,43 +8,32 @@
 using namespace std;
 namespace py=pybind11;
 
-LinearRegression::LinearRegression()
-{
-}
+LinearRegression::LinearRegression(){}
 
-void LinearRegression::fit(Matrix X, Matrix y)
-{
+void LinearRegression::fit(Matrix X, Matrix Y) {
     // Me llegan los metros cuadrados en X y en Y los precios 
-    int row_size_X = X.rows();
-	int col_size_X = X.cols();
+    //int row_size_X = X.rows();
+	//int col_size_X = X.cols();
 
-    int row_size_Y = Y.rows();
-	int col_size_Y = Y.cols();
+    //int row_size_Y = Y.rows();
+	//int col_size_Y = Y.cols();
 
 
     // Calculo B
     Vector means_X = X.colwise().mean();
     Vector means_Y = Y.colwise().mean();
 
-    X.rowwise() -= means_X;
-    Y.rowwise() -= means_Y;
-
-    Matrix squares = X.pow(2);
-    Vector squareSums = squares.colwise().sum();
-    double b1_denominator = squareSums[0];
+    X.rowwise() -= means_X.transpose();
+    Y.rowwise() -= means_Y.transpose();
 
 
-    Matrix multiplicationSums = Y.transpose() * X;
-    double b1_nominator = multiplicationSums[0];
+    Matrix XtX = X.transpose()*X;
+    Matrix XtY = X.transpose()*Y;
 
-    double b1 = b1_nominator/b1_denominator;
-
+    //y1 = b1*x1 + b2*x2 + b3*x3 + ... + bn*xn
+    Vector b = XtX.fullPivLu().solve(XtY);
 }
 
-
-Matrix LinearRegression::predict(Matrix X)
-{
-    auto ret = MatrixXd::Zero(X.rows(), 1);
-
-    return ret;
+Matrix LinearRegression::predict(Matrix X)  {
+    return X*b;
 }
